9f911327388e7e4220efa550d0f7c3f6519ec9b3
Vijendra Mishra <iamonetoughguy@gmail.com>
2020-04-05 16:10:57 +0530

    Changes for Object composition

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ const compose2 = f => g => x => f(g(x));
 const double = n => n * 2;
 const inc = n => n + 1;
 console.log(compose2(double)(inc)(3));
+
+// Objects can be easliy composed
+
+const a = "a";
+const oA = { a };
+
+const b = "b";
+const oB = { b };
+
+const c = { ...oA, ...oB };
+const d = Object.assign({}, oA, oB);
+
+console.log(c, d);
